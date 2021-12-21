f5e69ea8bb7b9cb57a31e0428eaa0e0f80014e94
RafalDziuba <72468641+RafalDziuba@users.noreply.github.com>
2021-12-21 15:36:28 +0100

    Working popup

--- src/main.js
+++ src/main.js
@@ -8,64 +8,59 @@ const showCars = () => {
         });
 };
 
-    const form = document.getElementById('carForm');
+const form = document.getElementById('carForm');
 
-    const sentForm = (e) => {
+const sentForm = (e) => {
 
-        e.preventDefault();
+    e.preventDefault();
 
-        const totalField = document.getElementById('total');
-        const yearField = document.getElementById('year');
-        const mileageField = document.getElementById('mileage');
-        const brandField = document.getElementById('brand');
-        const maintenanceField = document.getElementById('maintenance').value;
-        const coolingField = document.getElementById('cooling').value;
-        const exhaustField = document.getElementById('exhaust').value;
-        const brakeField = document.getElementById('brake').value;
-        const electricalField = document.getElementById('electrical').value;
-        const engineField = document.getElementById('engine').value;
-        const bodyField = document.getElementById('body').value;
-        const suspensionField = document.getElementById('suspension').value;
+    const totalField = document.getElementById('total');
+    const yearField = document.getElementById('year');
+    const mileageField = document.getElementById('mileage');
+    const brandField = document.getElementById('brand');
+    const maintenanceField = document.getElementById('maintenance').value;
+    const coolingField = document.getElementById('cooling').value;
+    const exhaustField = document.getElementById('exhaust').value;
+    const brakeField = document.getElementById('brake').value;
+    const electricalField = document.getElementById('electrical').value;
+    const engineField = document.getElementById('engine').value;
+    const bodyField = document.getElementById('body').value;
+    const suspensionField = document.getElementById('suspension').value;
 
-        let dataToSend = {
-            repair: {
-                year: yearField.value,
-                total_amount: totalField.value,
-                mileage: mileageField.value,
-                brand: brandField.value,
-                    items: [{
-                        ...(maintenanceField != '' && {maintenance: maintenanceField }),
-                        ...(coolingField != '' && {cooling_system: coolingField }),
-                        ...(exhaustField != '' && {exhaust_system: exhaustField }),
-                        ...(brakeField != '' && {brake_system: brakeField }),
-                        ...(electricalField != '' && {electrical_system: electricalField }),
-                        ...(engineField != '' && {engine: engineField }),
-                        ...(bodyField != '' && {body: bodyField }),
-                        ...(suspensionField != '' && {suspension: suspensionField })
-                 }]
+const repair ={
+        year: yearField.value,
+        total_amount: totalField.value,
+        mileage: mileageField.value,
+        brand: brandField.value,
+        items_attributes: [
+            (maintenanceField != '' && {amount: maintenanceField, category: 'maintenance'}),
+            (coolingField != '' && {amount: coolingField, category: 'cooling_system'}),
+            (exhaustField != '' && {amount: exhaustField, category: 'exhaust_system'}),
+            (brakeField != '' && {amount: brakeField, category: 'brake_system'}),
+            (electricalField != '' && {amount: electricalField, category: 'electrical_system'}),
+            (engineField != '' && {amount: engineField, category: 'engine'}),
+            (bodyField != '' && {amount: bodyField, category: 'body'}),
+            (suspensionField != '' && {amount: suspensionField, category: 'suspension'})]
+}
 
-            }
-        }
+axios({
+        method: "post",
+        url: 'https://car-service-api-app.herokuapp.com/repairs',
+        data: {
+            repair
+            },
+        headers: {
+            "Content-Type": "application/json"
+        },
+    })
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
 
-        axios({
-                method: "post",
-                url: 'https://car-service-api-app.herokuapp.com/repairs',
-                data: {
-                    dataToSend
-                },
-                headers: {
-                    "Content-Type": "application/json"
-                },
-            })
-            .then(function (response) {
-                console.log(response);
-            })
-            .catch(function (err) {
-                console.log(err);
-            });
-
-    }
+}
 
 
-    form.addEventListener('submit', sentForm);
-
+form.addEventListener('submit', sentForm);
--- src/stats.js
+++ src/stats.js
@@ -5,10 +5,10 @@ const pageNumbers = document.getElementById('pagination');
 
 
 const showOrders = () => {
-    axios.get('https://car-service-api-app.herokuapp.com/repairs')
+    axios.get('https://car-service-api-app.herokuapp.com/repairs\?BYPASS_AUTHENTICATION\=true')
         .then((res) => {
 
-
+            console.log(res.data)
             res.data.forEach(element => {
 
                 const orderWrap = document.createElement('div');
@@ -70,21 +70,86 @@ const showOrders = () => {
                 allOrders[i].addEventListener('click', () => {
 
                     const popup = document.createElement('div');
-                    const popupText = document.createElement('p');
                     const popupMenu = document.createElement('div');
+
                     const closeContainer = document.createElement('div');
                     const closeBtn = document.createElement('button');
+                    const popupTextContainer = document.createElement('div');
+
+                    const popupText = document.createElement('p');
+                    const popupText2 = document.createElement('p');
+                    const popupText3 = document.createElement('p');
+                    const popupText4 = document.createElement('p');
+                    const popupText5 = document.createElement('p');
+                    const popupText6 = document.createElement('p');
+                    const popupText7 = document.createElement('p');
+
+                    const popupTextPrice = document.createElement('p');
+                    const popupTextPrice2 = document.createElement('p');
+                    const popupTextPrice3 = document.createElement('p');
+                    const popupTextPrice4 = document.createElement('p');
+                    const popupTextPrice5 = document.createElement('p');
+                    const popupTextPrice6 = document.createElement('p');
+                    const popupTextPrice7 = document.createElement('p');
+
+                    const popupOrdersInfo = document.createElement('div');
+                    const popupOrdersInfo2 = document.createElement('div');
+                    const popupOrdersInfo3 = document.createElement('div');
+                    const popupOrdersInfo4 = document.createElement('div');
+                    const popupOrdersInfo5 = document.createElement('div');
+                    const popupOrdersInfo6 = document.createElement('div');
+                    const popupOrdersInfo7 = document.createElement('div');
+
                     section.appendChild(popup);
                     popup.appendChild(popupMenu);
+                    popup.appendChild(popupTextContainer);
                     popupMenu.appendChild(closeContainer);
                     closeContainer.appendChild(closeBtn);
-                    popup.appendChild(popupText);
+                    popupTextContainer.appendChild(popupOrdersInfo);
+                    popupTextContainer.appendChild(popupOrdersInfo2);
+                    popupTextContainer.appendChild(popupOrdersInfo3);
+                    popupTextContainer.appendChild(popupOrdersInfo4);
+                    popupTextContainer.appendChild(popupOrdersInfo5);
+                    popupTextContainer.appendChild(popupOrdersInfo6);
+                    popupTextContainer.appendChild(popupOrdersInfo7);
+                    popupOrdersInfo.appendChild(popupText);
+                    popupOrdersInfo2.appendChild(popupText2);
+                    popupOrdersInfo3.appendChild(popupText3);
+                    popupOrdersInfo4.appendChild(popupText4);
+                    popupOrdersInfo5.appendChild(popupText5);
+                    popupOrdersInfo6.appendChild(popupText6);
+                    popupOrdersInfo7.appendChild(popupText7);
 
+                    popupOrdersInfo.appendChild(popupTextPrice);
+                    popupOrdersInfo2.appendChild(popupTextPrice2);
+                    popupOrdersInfo3.appendChild(popupTextPrice3);
+                    popupOrdersInfo4.appendChild(popupTextPrice4);
+                    popupOrdersInfo5.appendChild(popupTextPrice5);
+                    popupOrdersInfo6.appendChild(popupTextPrice6);
+                    popupOrdersInfo7.appendChild(popupTextPrice7);
+
+
+                    // popup.appendChild(popupOrdersInfo);
+
+                    popupOrdersInfo.classList.add('popup-orders-info')
+                    popupOrdersInfo2.classList.add('popup-orders-info')
+                    popupOrdersInfo3.classList.add('popup-orders-info')
+                    popupOrdersInfo4.classList.add('popup-orders-info')
+                    popupOrdersInfo5.classList.add('popup-orders-info')
+                    popupOrdersInfo6.classList.add('popup-orders-info')
+                    popupOrdersInfo7.classList.add('popup-orders-info')
+                    popupTextContainer.classList.add('orders-info-container');
                     popupMenu.classList.add('popup-menu');
                     closeContainer.classList.add('close-btn')
                     closeBtn.classList.add('close')
                     popup.classList.add('order-popup');
                     popupText.classList.add('popup-info');
+                    popupText2.classList.add('popup-info');
+                    popupText3.classList.add('popup-info');
+                    popupText4.classList.add('popup-info');
+                    popupText5.classList.add('popup-info');
+                    popupText6.classList.add('popup-info');
+                    popupText7.classList.add('popup-info');
 
                     closeBtn.textContent = 'X';
                     // console.log(res.data[i].id);
@@ -94,13 +159,78 @@ const showOrders = () => {
                         section.removeChild(popup);
                     }
 
-                    popupText.textContent = res.data[i].items.map((item) => `${item.category}: ${item.amount}`)
+
+                    if (res.data[i].items[0] === undefined) {
+                        popupText.textContent = 'None';
+                        popupTextPrice.textContent = '0zł';
+                    } else {
+                        popupText.textContent = res.data[i].items[0].category + ':';
+                        popupTextPrice.textContent = res.data[i].items[0].amount + 'zł';
+                    };
+
+
+                    if (res.data[i].items[1] === undefined) {
+                        popupText2.textContent = 'None';
+                        popupTextPrice2.textContent = '0zł';
+                    } else {
+                        popupText2.textContent = res.data[i].items[1].category + ':';
+                        popupTextPrice2.textContent = res.data[i].items[1].amount + 'zł';
+                    };
+
+
+                    if (res.data[i].items[2] === undefined) {
+                        popupText3.textContent = 'None';
+                        popupTextPrice3.textContent = '0zł';
+                    } else {
+                        popupTex3.textContent = res.data[i].items[2].category + ':';
+                        popupTextPrice3.textContent = res.data[i].items[2].amount + 'zł';
+                    };
+
+
+                    if (res.data[i].items[3] === undefined) {
+                        popupText4.textContent = 'None';
+                        popupTextPrice4.textContent = '0zł';
+                    } else {
+                        popupText4.textContent = res.data[i].items[3].category + ':';
+                        popupTextPrice4.textContent = res.data[i].items[3].amount + 'zł';
+                    };
+
+
+                    if (res.data[i].items[4] === undefined) {
+                        popupText5.textContent = 'None';
+                        popupTextPrice5.textContent = '0zł';
+                    } else {
+                        popupText5.textContent = res.data[i].items[4].category + ':';
+                        popupTextPrice5.textContent = res.data[i].items[4].amount + 'zł';
+                    };
+
+
+                    if (res.data[i].items[5] === undefined) {
+                        popupText6.textContent = 'None';
+                        popupTextPrice6.textContent = '0zł';
+                    } else {
+                        popupText6.textContent = res.data[i].items[5].category + ':';
+                        popupTextPrice6.textContent = res.data[i].items[5].amount + 'zł';
+                    };
+
+
+                    if (res.data[i].items[6] === undefined) {
+                        popupText7.textContent = 'None';
+                        popupTextPrice7.textContent = '0zł';
+                    } else {
+                        popupText7.textContent = res.data[i].items[6].category + ':';
+                        popupTextPrice7.textContent = res.data[i].items[6].amount + 'zł';
+                    };
+
+
+                    // popupTextPrice.textContent = res.data[i].items[2].amount
+                    console.log(res.data[i].id)
                     closeBtn.addEventListener('click', closePopup);
                 });
             }
 
             let currentPage = 1;
-            let rows = 2;
+            let rows = 6;
             const displayList = (items, wrapper, rowsPerPage, page) => {
                 wrapper.innerHTML = "";
                 page--;
